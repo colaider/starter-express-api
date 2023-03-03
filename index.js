41ed8b1f234ec83aeb371988876c1d89e145764b
colaider <45285561+colaider@users.noreply.github.com>
2023-03-03 17:16:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const express = require('express');
+const app = express();
+const port = 5000;
+var pool = require('./routes/pool')
+
+
+
+function write(tog,temp, req,res){
+  let date_ob = new Date();
+  let date = ("0" + date_ob.getDate())
+  console.log(date)
+  
+  // pool.query("DELETE FROM Toggle",(err, result)=>{
+
+  // })
+
+  // pool.query(`INSERT INTO Toggle(id,toggle,date,temperature)
+  // VALUES(${1}, ${0},"${date}", ${56})`,(error,result) =>{
+  //          if(error){
+  //              res.send(error)
+  //          }else{
+  //           res.send("hui")
+  //          }
+  // })
+
+  pool.query(`UPDATE Toggle SET id=1, toggle=${tog},  date="${date}", temperature=${temp} WHERE id = 1`,(error,result) =>{
+    if(error){
+      res.send(error);
+    }else{
+      res.send("Ui <<-->>")
+    }
+  })
+}
+
+app.get('/write/:tog/:temp', (req,res) =>{
+  write(req.params.tog, req.params.temp, req, res)
 })
-app.listen(process.env.PORT || 3000)
+
+
+app.get('/get', (req, res) => {
+  // res.send('Hello World!');
+  pool.query('SELECT * FROM Toggle',(error,result) =>{
+    if(error){
+        res.send(error);
+    }else{
+        res.send(result)
+    }
+})
+})
+
+
+app.listen(process.env.port || port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
